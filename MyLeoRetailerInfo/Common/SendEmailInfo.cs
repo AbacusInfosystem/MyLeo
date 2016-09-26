5627e2d17e5ee9518174a582fbdfc9d3d3ccf259
sanchita-sawant <sanchitasawant1493@gmail.com>
2016-09-26 19:00:11 +0530

    Common method added for Send Email

---
+++ MyLeoRetailerInfo/Common/SendEmailInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeoRetailerInfo.Common
+{
+    public class SendEmailInfo
+    {
+        public int ID { get; set; }
+
+        public string To_Email_Id { get; set; }
+
+        public string CC_Email_Id { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+    }
+}
