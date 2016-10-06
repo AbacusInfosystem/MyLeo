8aaa708abe6ce078faacf7b088425fefa04b1606
kunal-shinde <kunal.shinde@abacusinfosystem.com>
2016-10-06 19:25:10 +0530

    free

--- MyLeoRetailerInfo/Receivable/ReceivableInfo.cs
+++ MyLeoRetailerInfo/Receivable/ReceivableInfo.cs
@@ -51,14 +51,6 @@ namespace MyLeoRetailerInfo.Receivable
            set;
        }
 
-       public int Branch_ID
-       {
-           get;
-           set;
-       }
-
-       public string Branch_Name { get; set; }
-
        public int Receivable_Status
        {
            get;
