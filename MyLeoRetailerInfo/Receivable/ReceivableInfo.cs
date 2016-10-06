bbe48cf98a7dafb0751555be9571ed94c753ff6b
gauravi-lad <lad.gauravi24@gmail.com>
2016-10-06 21:12:09 +0530

    free

--- MyLeoRetailerInfo/Receivable/ReceivableInfo.cs
+++ MyLeoRetailerInfo/Receivable/ReceivableInfo.cs
@@ -19,6 +19,17 @@ namespace MyLeoRetailerInfo.Receivable
            set;
        }
 
+       public int Branch_ID
+       {
+           get;
+           set;
+       }
+
+       public string Branch_Name
+       {
+           get;
+           set;
+       }
 
        public int Sales_Invoice_Id
        {
