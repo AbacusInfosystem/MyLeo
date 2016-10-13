f1518ba5b79522db77786684bf47991a1038b00e
Vivek Mogare <vivekanand.mogare@abacusinfosystem.com>
2016-10-13 19:21:31 +0530

    work on edit page

--- MyLeoRetailer/Scripts/Modules/Employee/employee_methods.js
+++ MyLeoRetailer/Scripts/Modules/Employee/employee_methods.js
@@ -2,12 +2,17 @@
  
 function ResetForm() {
     $('#frmEmployee').each(function () {
-        this.reset(); 
+       
+      
+        this.reset();
+       
     });
     //$('[data-id="drpDesignation"]').html("Select Designation" + " <span class=\"caret\"></span>");
     //$('[data-id="drpGender"]').html("Select Gender" + " <span class=\"caret\"></span>"); 
     //$('[data-id="drpBranch"]').html("Select Branch" + " <span class=\"caret\"></span>");
 }
+
+
 
 
 
