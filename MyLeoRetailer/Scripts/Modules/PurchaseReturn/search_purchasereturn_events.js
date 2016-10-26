816bc099c205f12f0a387254dc84e80c07559121
Vivek Mogare <vivekanand.mogare@abacusinfosystem.com>
2016-10-26 13:47:33 +0530

    fixed purchase return management lookup

--- MyLeoRetailer/Scripts/Modules/PurchaseReturn/search_purchasereturn_events.js
+++ MyLeoRetailer/Scripts/Modules/PurchaseReturn/search_purchasereturn_events.js
@@ -76,4 +76,10 @@ $(function () {
 
     });
 
+    //Added By Vinod Mane on 26/10/2016
+    $(document).on("change", "#hdnPurchaseReturnId", function () {
+       
+        Get_Purchase_Returns();
+    });
+    //End
 });
