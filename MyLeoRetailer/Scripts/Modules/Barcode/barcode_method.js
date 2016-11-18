9c807fc322cf3a766905925d16dddf881d220937
gauravi-lad <lad.gauravi24@gmail.com>
2016-11-18 12:10:22 +0530

    Add Barcode Module

--- MyLeoRetailer/Scripts/Modules/Barcode/barcode_method.js
+++ MyLeoRetailer/Scripts/Modules/Barcode/barcode_method.js
@@ -1,23 +1,20 @@
 ﻿
 
-function Get_Employees() {
-    var eViewModel =
+function Get_Barcodes() {
+
+    var bViewModel =
 		{
-		    Filter: {
+		    Barcode: {
 
-		        Employee: $("[name='Filter.Employee']").val()
-		    },
-		    Grid_Detail: {
-
-		        Pager: Set_Pager($("#divEmployeePager"))
-		    }
+		        Product_SKU: $("[name='Barcode.Product_SKU']").val(),
+		    },		    
 		}
 
     $.ajax({
 
-        url: "/Employee/Get_Employees",
+        url: "/Barcode/Get_Barcodes",
 
-        data: JSON.stringify(eViewModel),
+        data: JSON.stringify(bViewModel),
 
         dataType: 'json',
 
@@ -25,153 +22,55 @@ function Get_Employees() {
 
         contentType: 'application/json',
 
-        success: function (response) {
-            var obj = $.parseJSON(response);
+        success: function (response) {           
 
-            Bind_Grid(obj, "Employee_List");
-             
-            $("#divEmployeePager").html(obj.Grid_Detail['Pager']['PageHtmlString']);
+            var data = $.parseJSON(response);
+
+            Bind_Get_Barcodes_Data(data);
         }
     });
 }
 
-function Bind_Payable_Grid_Items(data) {
 
-    ClearPayableData();
 
-    $("#tblPayableItems").html("");
+function Bind_Get_Barcodes_Data(data) {
 
-    var htmlText = "";
+    var tblHTML = "";
 
-    $("#hdnPayable_Item_Id").val(data.Payable.Payable_Item_Id),
+    $('#tblBarcode tbody tr').remove();
 
-    $("#hdnPayable_Id").val(data.Payable.Payable_Id),
+    if (data.Barcode.Barcodes.length > 0) {
 
-    $("#hdnPurchase_Invoice_Id").val(data.Payable.Purchase_Invoice_Id),
+        for (var i = 0; i < data.Barcode.Barcodes.length; i++) {
 
-    $("#txtTotal_Amount").val(data.Payable.Total_Amount),
+            tblHTML += "<tr>";
 
-    $("#txtAmount_due").val(data.Payable.Balance_Amount);
+            tblHTML += "<td><label class='switch'><input checked='' value='1' type='checkbox'><span></span></label>";
+            
+            tblHTML += "<input type='hidden' id='hdnId" + i + "' name='Barcode.Barcodes[" + i + "].Product_SKU_Barcode_Id' value='" + data.Barcode.Barcodes[i].Product_SKU_Barcode_Id + "' />";
+            
+            tblHTML += "</td>";
 
-    $("#txtFinal_amount").val(data.Payable.Balance_Amount);
+            tblHTML += "<td>" + data.Barcode.Barcodes[i].Product_SKU + "</td>";
 
-    if (data.Payables.length > 0) {
+            tblHTML += "<td>" + data.Barcode.Barcodes[i].Product_SKU +"-"+ data.Barcode.Barcodes[i].Product_Barcode_Counter + "</td>";
 
-        htmlText += "<tr>";
+            tblHTML += "<td>";
+            
+            tblHTML += "<img src='" + (data.Barcode.Barcodes[i].Barcode_Image_Url != null ? data.Barcode.Barcodes[i].Barcode_Image_Url : "") + "' style='width:200px'/>";
+            
+            tblHTML += "<input type='hidden' id='hdn_Barcode' class='form-control' name='Barcode.Barcodes[" + i + "].Product_Barcode' value='" + (data.Barcode.Barcodes[i].Product_Barcode == null ? '' : data.Barcode.Barcodes[i].Product_Barcode) + "' />";
 
-        htmlText += "<th>Payment mode</th>";
+            tblHTML += "</td>";
+            
+            tblHTML += "</tr>";
 
-        htmlText += "<th>Paid Amount</th>";
+        }
 
-        htmlText += "<th>Vendor Employee Name</th>";
-
-        htmlText += "<th>Credit Card no</th>";
-
-        htmlText += "<th>Debit Card no</th>";
-
-        htmlText += "<th>Cheque No</th>";
-
-        htmlText += "<th>Cheque Date</th>";
-
-        htmlText += "<th>Bank Name</th>";
-
-        htmlText += "<th>Credit note no</th>";
-
-        htmlText += "<th>Credit note amount</th>";
-
-        htmlText += "<th>Discount %</th>";
-
-        htmlText += "<th>Discount amount</th>";
+        $('#tblBarcode tbody').append(tblHTML);
 
     }
-
-    htmlText += "</tr>";
-
-    for (i = 0; i < data.Payables.length; i++) {
-
-        htmlText += "<tr>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Payment_Mode1 == null ? "" : data.Payables[i].Payment_Mode1;
-
-        htmlText += "<input type='hidden' id='hdnPayment_Mode" + data.Payables[i].Payable_Item_Id + "' value='" + data.Payables[i].Payment_Mode + "'/>";
-
-        htmlText += "<input type='hidden' id='hdnPaid_Amount" + data.Payables[i].Payable_Item_Id + "' value='" + data.Payables[i].Paid_Amount + "'/>";
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Paid_Amount == null ? "" : data.Payables[i].Paid_Amount;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Vendor_Employee_Name == null ? "NA" : data.Payables[i].Vendor_Employee_Name;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Credit_Card_No == null ? "" : data.Payables[i].Credit_Card_No;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Debit_Card_No == null ? "" : data.Payables[i].Debit_Card_No;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Cheque_No == null ? "" : data.Payables[i].Cheque_No;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Cheque_Date == "1999-01-01" ? "NA" : data.Payables[i].Cheque_Date.substring(0, 10);
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Bank_Name == null ? "" : data.Payables[i].Bank_Name;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Credit_Note_No == null ? "NA" : data.Payables[i].Credit_Note_No;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Credit_Note_Amount == 0 ? "NA" : data.Payables[i].Credit_Note_Amount;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Discount_Percentage == 0 ? "NA" : data.Payables[i].Discount_Percentage;
-
-        htmlText += "</td>";
-
-        htmlText += "<td>";
-
-        htmlText += data.Payables[i].Discount_Amount == 0 ? "NA" : data.Payables[i].Discount_Amount;
-
-        htmlText += "</td>";
-
-        htmlText += "</tr>";
-    }
-
-
-    $('#tblPayableItems').html(htmlText);
+      
+    Friendly_Messages(data);
 }
-
 
